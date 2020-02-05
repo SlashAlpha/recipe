42ca3f0e7b0ed1b48c4106310f16b744781337d0
slashuzero <duprat.c@live.com>
2020-02-05 22:51:58 +0100

    Adding Recipe project to fork

---
+++ src/main/java/slash/process/recipe/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package slash.process.recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index"})
+
+    public String getIndexPage() {
+
+        System.out.println("Some message to say...");
+        return "index";
+    }
+}
---
+++ src/test/java/slash/process/recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package slash.process.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
