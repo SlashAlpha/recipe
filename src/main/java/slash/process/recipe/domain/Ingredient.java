d29879283575c49a6fc58be03665f5fe9afea408
slashuzero <duprat.c@live.com>
2020-02-05 23:52:28 +0100

    Adding Unit of Measure for ingredients

---
+++ src/main/java/slash/process/recipe/domain/Ingredient.java
@@ -0,0 +1,58 @@
+package slash.process.recipe.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @OneToOne(fetch = FetchType.EAGER)
+    UnitOfMeasure unitOfMeasure;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descriptions;
+    private BigDecimal amount;
+    @ManyToOne
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
+
+    public String getDescriptions() {
+        return descriptions;
+    }
+
+    public void setDescriptions(String descriptions) {
+        this.descriptions = descriptions;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
